7c1e68f7c22804b35ddcf2b058072d320098662c
sekwangoh <osktr5@gmail.com>
2021-06-15 17:40:13 +0900

    tennisapp start create views

---
+++ tennisapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from main.views import index,test
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('test/', test)
+]
+
+
