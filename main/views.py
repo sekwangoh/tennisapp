f1d021591da86275df890c14c309980617dd67e9
sekwangoh <osktr5@gmail.com>
2021-06-22 07:18:04 +0900

    chart.js 추가

--- main/views.py
+++ main/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
 def index(request):
+    print(request)
+    print(type(request))
+    print(dir(request))
+    print('-------')
+    print(request.POST)
+    
     return render(request, 'index.html')
 
+
 def test(request):
-    return render(request, 'test.html')
+    d = {"name":"sekwang", "age":10}
+    return JsonResponse(d)
+    #return HttpResponse('hello, world')
